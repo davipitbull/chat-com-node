2250e81e6294ed229d5f3dcd41d7c21b54c9ea78
DaviPitbull <davidestudosdev@gmail.com>
2024-09-12 20:30:51 -0300

    front no ar

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-com-node.onrender.com")
     websocket.onmessage = processMessage
 }
 
